59334f034a7f4357e5c36fc8df6c213cf8fc69a3
Coury Ditch <cditch@DDGPNR32.officescapes.dom>
2016-02-21 18:08:02 -0700

    A Rotor Class
    You can create a new rotor. Also rotate it, reset it, and hash it's
    current state to see what encoded letter corresponds to it's plaintext.

--- Rotor.rb
+++ Rotor.rb
@@ -3,18 +3,20 @@ class Rotor
 		#calling dup so the original rotor remains unrotated
         @rotor_array = rotor_array.dup 
 		#Creating another to aid the reset, no reason found to make it clone rather than dup though.
-        @reset_rotor = rotor_array.clone 	
+        {@reset_rotor = @rotor_array}.freeze
+		@position = 1
     end
     
 	#Resets the wheel to it's orginal positon.
     def reset
-        @rotor_array = @reset_rotor
+        @rotor_array = @reset_rotor.dup
     end
     
 	#Rotates the wheel. Moves the last letter to the beginning of the array.
     def rotate
         @rotor_array.unshift(@rotor_array.pop)
         print @rotor_array
+		@position += 1
     end
 	
 	#Places the current state of the rotor into a hash corresponding to it's plaintext equivalent.
@@ -29,6 +31,9 @@ class Rotor
   
       return rotor_hash
     end
+	
+	def position?
+		prints "The wheel has rotated #{@position / 26} times and is at position #{@position % 26}"
 end
 
 $i_1930 = ["E","K","M","F","L","G","D","Q","V","Z","N","T","O","W","Y","H","X","U","S","P","A","I","B","R","C","J"]
