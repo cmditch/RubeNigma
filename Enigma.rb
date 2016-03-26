ce16adfaf0b7325a9d55b6899c89acf6086ba46c
cmditch <cmditch@gmail.com>
2016-03-26 02:08:35 -0600

    Cleaned up the code a bunch. Altered UX and bugs for rotor setting.

--- Enigma.rb
+++ Enigma.rb
@@ -9,17 +9,14 @@
 #
 
 class Rotor
-Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!?;:-.,'\" 1234567890"
-
+  Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!?;:-.,'\" 1234567890"
     def initialize(rotor_string)
         @rotor_array = rotor_string.split("").dup  #Calling dup so the original rotor remains unrotated
         #Creating another to aid the reset, no reason found to make it clone rather than dup though.
         @reset_rotor = @rotor_array.dup
         @rotor_hash = Hash.new
-        @current_letter = "A"
         @position = 0
     end
-    
     
     #Places the current state of the rotor into a hash corresponding to it's plaintext equivalent.
     def hashit
@@ -27,8 +24,8 @@ Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!?;:-.,'\" 12345678
       Chars.split("").each do |x|
        @rotor_hash[x] = @rotor_array[n]
        n += 1
-       end  
-       @rotor_hash
+      end  
+      @rotor_hash
     end 
     
     #Rotates the wheel. Moves the last letter to the beginning of the array.
@@ -68,14 +65,44 @@ def main
   $rotor_ii = Rotor.new( "AJDKSIR1t2,3m4c5q6g7z8n9p0':yfvoeU.XBLHWTsir!uxbM;-CQG?Z\"NPY FVOEajdklhw")
   $rotor_iii = Rotor.new("BD FHJLCPRTXyeiwg1a2k3m4'5u6s7q8o9V0Z:N?YEIWjlcp-rt;GAKMU.\"SQOb!dfhx,vzn")
   $reflector = Rotor.new(Rotor::Chars.reverse)
-
+  $cipher_letters = []
   $nonce_i = 0
   $nonce_ii = 0
 
+  #Sends each letter of your message through the rotor circutiry and back.
+  def cipherize(msg)
+    msg.split("").each do |n|
+      n = $rotor_i.hashit.values_at(n).join
+      n = $rotor_ii.hashit.values_at(n).join
+      n = $rotor_iii.hashit.values_at(n).join
+      n = $reflector.hashit.values_at(n).join
+      n = $rotor_iii.hashit.invert.values_at(n).join
+      n = $rotor_ii.hashit.invert.values_at(n).join
+      n = $rotor_i.hashit.invert.values_at(n).join
+      $cipher_letters << n
+      $rotor_iii.rotate 
+      $nonce_i += 1
+      $nonce_ii += 1
+
+      #Arbitrary values to rotate each rotor
+      if $nonce_ii / rand(20) == 1
+        $rotor_ii.rotate
+        $nonce_ii = 0
+      end
+        
+      if $nonce_i / rand(50) == 1
+        $rotor_i.rotate
+        $nonce_i = 0
+      end
+
+    end
+    $cipher_letters.join
+  end
+  
+  #Requests to run again or exits, each adds 60 blank lines as a dumb way to clear terminal.
   def run_again?
       print "\n\nRun the 'Virtual Enigma' again?\n\n(1) Yes\n(2) No\n"
       repeat = gets.chomp.to_i
-    #Requests to run-again? or exit, and clears screen each time (adds 60 lines).
     case repeat
       when 1
         60.times do print "\n" end
@@ -89,50 +116,29 @@ def main
     end
   end
 
-  def cipherize(msg_array)
-    msg_array.each do |into_rotor_i|
-      into_rotor_ii = $rotor_i.hashit.values_at(into_rotor_i).join
-      into_rotor_iii = $rotor_ii.hashit.values_at(into_rotor_ii).join
-      into_reflector = $rotor_iii.hashit.values_at(into_rotor_iii).join
-      back_through_rotor_iii = $reflector.hashit.values_at(into_reflector).join
-      back_through_rotor_ii = $rotor_iii.hashit.invert.values_at(back_through_rotor_iii).join
-      back_through_rotor_i = $rotor_ii.hashit.invert.values_at(back_through_rotor_ii).join
-      $cipher_letters << $rotor_i.hashit.invert.values_at(back_through_rotor_i).join
-      $rotor_iii.rotate 
-      $nonce_i += 1
-      $nonce_ii += 1
-
-      if $nonce_ii / 13 == 1
-        $rotor_ii.rotate
-        $nonce_ii = 0
-      end
-        
-      if $nonce_i / 26 == 1
-        $rotor_i.rotate
-        $nonce_i = 0
-      end
-    end
-    $cipher_letters.join
-  end
-
-  $cipher_letters = []
-  
   60.times do print "\n" end
   puts "\n\n\n            Virtual Enimga\n"
   puts "          Created by C.Ditch"
   puts "\n\n Set each rotors starting letter. Example 'tyx' : "
-  rotors_placement = gets.chomp.upcase.split("")
+  rotors_placement = gets.chomp
+
+  if rotors_placement.length != 3
+    print "\nThree letters!"
+    gets
+    main
+  else
+    rotors_placement = rotors_placement.upcase.split("")
+  end
 
   $rotor_i.set($rotor_i.rotor_array.index(rotors_placement[0]))
   $rotor_ii.set($rotor_ii.rotor_array.index(rotors_placement[1]))
   $rotor_iii.set($rotor_iii.rotor_array.index(rotors_placement[2]))
-  $reflector.set(1)
+  $reflector.hashit
 
   print "\n\nEnter your message:\n"
-  user_input = gets.chomp.split("")
+  user_input = gets.chomp
 
-  print "\nOutput:\n"
-  print cipherize(user_input)
+  print "\nOutput:\n" + cipherize(user_input)
   
   run_again?
 
